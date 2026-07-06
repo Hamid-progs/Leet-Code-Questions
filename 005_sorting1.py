fd0d036787f246fb7bf0fd3fcf98a0818ed063b1
MUHAMMAD HAMID RAZA <m.hamidrazashahid321@gmail.com>
2026-07-06 19:57:06 +0500

    Insertion sort added

--- 005_sorting1.py
+++ 005_sorting1.py
@@ -34,3 +34,25 @@ def BubbleSort(l = [13,46,24,52,20,9]):
     print(l)
 
 # BubbleSort()
+
+# ---------------------------------------------------
+
+# Bubble sort 
+
+# time complexity for average and worst case is o(n^2)
+def BubbleSort(l = [13,46,24,52,20,9]):
+    didswap = 0  # this will be an indicatior if it remain zero no swaping occur hence array is sorted time compelxity o(n)
+    for i in range(1,len(l)):
+        for j in range(0,len(l)-i):
+            if l[j] > l[j+1]:
+                temp = l[j]
+                l[j] = l[j+1]
+                l[j+1] = temp
+                didswap = 1
+
+        if didswap == 0:
+            break    
+        
+    print(l)
+
+# BubbleSort()
