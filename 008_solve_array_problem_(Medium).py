fa1eb9c3ccf0ddf7c58d04f16bbf318f3d199c99
MUHAMMAD HAMID RAZA <m.hamidrazashahid321@gmail.com>
2026-07-31 19:58:43 +0500

    selecting majority element , also with Moore's Voting Algorithm
    Refactored solutions for array problems, including pair sum checks and sorting algorithms. Added majority element solutions.

--- 008_solve_array_problem_(Medium).py
+++ 008_solve_array_problem_(Medium).py
@@ -103,3 +103,57 @@ def sort_array2(nums = [0, 0, 1,1,1,2,1,2,0,0,0]):
 
 # sort_array2()
 
+# ----------------------------------------------------------
+
+# Problem Statement: 
+# Given an integer array nums of size n, return the majority element of the array.
+# The majority element of an array is an element that appears more than n/2 times in the array. 
+# The array is guaranteed to have a majority element.
+
+# ---Solution 1---
+# time complexity O(N) hashing approach
+# space complexity O(N) hash map
+def majority1(nums = [7, 0, 0, 1, 7, 7, 2, 7, 7]):
+    mp = {} 
+
+    for i in range(len(nums)):
+        if nums[i] not in mp:
+            mp[nums[i]] = 1
+        else:
+            mp[nums[i]] += 1
+
+        if len(nums)//2 < mp[nums[i]]:
+            print(nums[i])
+            break
+
+# majority1()
+
+
+# ---Solution 2---
+# time complexity is O(N) or O(2N)
+# space complexity is O(1) which is good
+def majority2(nums = [7, 0, 0, 1, 7, 7, 2, 7, 7]):
+    el = nums[0]
+    count = 1
+
+    for i in range(1,len(nums)):
+        if count == 0:
+            el = nums[i]
+            count = 1
+        elif nums[i] == el:
+            count += 1
+        else:
+            count -= 1
+
+    # verify the canidate
+    c = 0
+    for i in range(len(nums)):
+        if el == nums[i]:
+            c += 1
+
+    if c > len(nums)//2:
+        print(el)
+    else:
+        print("NO majority element!")
+
+# majority2()
