9c96e8972d6684e831479c6ba3bf54154e649e6e
MUHAMMAD HAMID RAZA <m.hamidrazashahid321@gmail.com>
2026-08-10 19:35:32 +0500

    Add second method for longest consecutive sequence
    Added a second method 'longestConsecutive2' to calculate the longest consecutive sequence using a set for O(N) time complexity.

--- leetcode Questions/128-Longest_Consecutive_sequence.py
+++ leetcode Questions/128-Longest_Consecutive_sequence.py
@@ -1,6 +1,7 @@
 # link: https://leetcode.com/problems/longest-consecutive-sequence
 
 class Solution:
+    # time complexity O(nlogn)
     def longestConsecutive(self, nums):
         if nums == []:
             return 0
@@ -24,4 +25,22 @@ class Solution:
         max_l = max(max_l,l)
         return max_l
 
+    # time  complexity O(N)
+    def longestConsecutive2(self, nums: List[int]) -> int:
+        st = set(nums)
+        if len(st) == 0 :
+            return 0
+            
+        longest = 0
+
+        for x in st:
+            if x-1 not in st:
+                cnt = 1
+                while x+1 in st:
+                    x += 1
+                    cnt += 1
+
+                longest = max(longest,cnt)
+
+        return longest
         
